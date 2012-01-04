bc78db1247be58e061c445612d9da82f3b79b7f9
mikera <mike.r.anderson.13@gmail.com>
2012-01-04 23:50:00 +0800

    misc

--- src/main/clojure/mc/image.clj
+++ src/main/clojure/mc/image.clj
@@ -5,6 +5,11 @@
   (:import [java.awt Image Graphics])
   (:import [java.awt.image BufferedImage]))
 
+(defn rgb [r g b]
+  (Colours/getRGBClamped (double r) (double g) (double b)))
+
+(defn argb [r g b]
+  (Colours/getARGBClamped (double a)  (double r) (double g) (double b)))
 
 (defn show 
   ([^Image img]
@@ -13,6 +18,36 @@
   ([^String title ^Image img]
 	  "Show an image in a new or existing frame with a given title"
 	  (ImageUtils/display title img)))
+
+(defn buffered-image [width height]
+  "Create a default buffered image of the specified size in pxels"
+  (Generator/newImage (int width) (int height)))
+
+(defn create-image [f width height] 
+  (let [bi ^BufferedImage (buffered-image width height)
+        xfactor (double (/ 1.0 width))
+        yfactor (double (/ 1.0 height))]
+	  (dotimes [y height]
+	    (dotimes[x width]
+	      (.setRGB bi x y (f (* x xfactor) (* y yfactor)))))
+    bi))
+
+(defmacro image-function-2D [form]
+  `(fn name#
+     ([^double ~'x ^double ~'y]
+       ~form)
+     ([^double ~'x ^double ~'y ^double ~'z]
+       (name# ~'x ~'y 0))))
+
+
+(defn sampler 
+  "Return a function that samples from a given image"
+  ([^BufferedImage image]
+    (let [width (.getWidth image)
+          height (.getHeight image)]
+    (image-function-2D 
+      (.getRGB (int (* x width)) (int (* y height)))))))
+
 
 
 
