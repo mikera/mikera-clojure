66b50ed7e469055b9057cba1de630bacb5925eba
mikera <mike.r.anderson.13@gmail.com>
2012-05-30 10:42:44 +0800

    Rickroll files

---
+++ src/main/clojure/golf/rickroll.clj
@@ -0,0 +1,10 @@
+(ns golf.rickroll)
+
+(let [r 
+      s str 
+      w \s 
+      r \n
+      n "\nNever gonna "
+      c (s n "give you up" r n "let you down" n "run" )])
+                                                 
+                                                 
