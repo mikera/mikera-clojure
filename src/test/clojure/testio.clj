a9a197cb20254f391907beba2cc8bba47a44dd57
mikera <Mike@Mike-VAIO>
2012-08-28 09:43:41 +0800

    Merge from master

--- src/test/clojure/testio.clj
+++ src/test/clojure/testio.clj
@@ -1,7 +1,7 @@
 (ns testio
-  (:use [mc.resource])
+  (:use mc.resource)
   (:use clojure.test))
 
-(deftest test-resource-file
-  (is (not (nil? (mc.resource/resource "euler/names.txt")))))
-
+(deftest test-arithmetic
+  (testing "Basic arithmetic"
+    (is (mc.resource/resource "euler/names.txt"))))
