2b2c92cb93d477662e0eac9e156980b4bbcb99fe
mikera <mike.r.anderson.13@gmail.com>
2012-09-16 21:58:57 +0800

    Update evolve.clj for clojure 1.4

--- src/main/clojure/mc/evolve/evolve.clj
+++ src/main/clojure/mc/evolve/evolve.clj
@@ -2,21 +2,13 @@
 	(:use [clojure.tools.macro])) 
 
 
-
-
-
-
-
 (def create (fn [] [(rand)]))
 
 (def mutate (fn [v] 
-			(let [c (count v)]
-				(if (= c 0)
-					('create)
-					(assoc v (rand-int (inc (count v))) (rand))
-				)
-			)
-		))
+	(let [c (count v)]
+		(if (= c 0)
+			('create)
+			(assoc v (rand-int (inc (count v))) (rand))))))
 
 (def popcount 10)
 
@@ -32,9 +24,7 @@
 		n (count vs)
 		r (quot n 2)
 		winners (take r sort_vs)]
-		(concat winners (map mutate (repeat (- n r) (nth winners (rand-int r))))
- 
-		))) 
+		(concat winners (map mutate (repeat (- n r) (nth winners (rand-int r))))))) 
 
 (defn best [] (first current)) 
 
@@ -42,6 +32,4 @@
 	(dotimes [n steps]
 		(do
 			(def current (step current))
-			(println (str (best)) )
-		)	
-))
+			(println (str (best))))))
